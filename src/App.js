e89834a215b0fa4d0f269b2b106ea9c763c8cd14
iwasaki2310 <118096073+iwasaki-2310@users.noreply.github.com>
2023-11-12 05:06:14 +0000

    JSONPlaceholderより情報取得

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import axios from "axios"; // axiosをインポート
 import "./styles.css";
 
 export default function App() {
-  return <div className="App"></div>;
+  const onClickUsers = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const onClickUser1 = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users/3")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <div className="App">
+      <button onClick={onClickUsers}>users</button>
+      <button onClick={onClickUser1}>id=1のuser</button>
+    </div>
+  );
 }
